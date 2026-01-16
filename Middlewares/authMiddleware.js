e586a80747d3de11db8bd06d9c707a28c6bd90df
rotneng <129745212+rotneng@users.noreply.github.com>
2026-01-16 12:35:42 +0100

    More Enhancements

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -2,24 +2,27 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   let token;
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
       req.user = decoded;
 
       next();
     } catch (error) {
       console.error(error);
-      res.status(401).json({ message: "Not authorized, token failed" });
+      return res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: "Not authorized, no token" });
+    return res.status(401).json({ message: "Not authorized, no token" });
   }
 };
 
@@ -27,7 +30,7 @@ const verifyAdmin = (req, res, next) => {
   if (req.user && req.user.role === "admin") {
     next();
   } else {
-    res.status(403).json({ message: "Not authorized as an admin" });
+    return res.status(403).json({ message: "Not authorized as an admin" });
   }
 };
 
